69e8d94404cae33477dd1b03ce2c91afa3028df3
Isabelle <isabellefotograf@gmail.com>
2024-05-06 05:09:32 +0200

    fixed the backend code to herosection input search, now we have to fix some ai feature to this

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
+import pa11y from "pa11y";
 import cors from "cors";
 
 const app = express();
-const port = 4000;
+const port = 5009;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+
+// create a route
+app.get("/pa11y/test", async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    res.status(400).json({ error: "Invalid URL" });
+  } else {
+    try {
+      const results = await pa11y(url);
+      res.status(200).json(results);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while testing the URL" });
+    }
+  }
+});
 
 // Server start
 app.listen(port, () => {
-  console.log(`Bankens backend körs på http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+
+// const app = express();
+// const port = 4000;
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Server start
+// app.listen(port, () => {
+//   console.log(`Bankens backend körs på http://localhost:${port}`);
+// });
