c4fb6be304b9280f212358a7c5ee46ff77ebe9d5
AdamFagerberg <Adam.fagerberg1@gmail.com>
2024-05-07 11:58:02 +0200

    backend

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import bodyParser from "body-parser";
 import express from "express";
 import pa11y from "pa11y";
 import cors from "cors";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 5009;
 
 // Middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 // create a route
 app.get("/pa11y/test", async (req, res) => {
@@ -28,24 +33,35 @@ app.get("/pa11y/test", async (req, res) => {
   }
 });
 
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+app.post("/scanner", async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    res.status(400).json({ error: "Invalid URL" });
+  } else {
+    const results = await pa11y(url);
+    const scan = JSON.stringify(results);
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content:
+            "These are the accesibility issues after a scan, could you make a list with the context and messages and add a solution to each one" +
+            scan,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const answer = completion.choices[0].message;
+    res.send(answer);
+  }
+});
+
 // Server start
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-
-// const app = express();
-// const port = 4000;
-
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// // Server start
-// app.listen(port, () => {
-//   console.log(`Bankens backend körs på http://localhost:${port}`);
-// });
